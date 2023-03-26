6797343cbddb08d6e036e57333ed4da8473a8525
hahyoungkim <hyk323@nyu.edu>
2023-03-26 19:05:30 -0400

    added animation + updated code to flip player in the direction it is moving

--- Platformer/Assets/Code/Player.cs
+++ Platformer/Assets/Code/Player.cs
@@ -9,6 +9,9 @@ public class Player : MonoBehaviour
     public int jumpForce = 650;
     int bulletSpeed = 600;
     private Rigidbody2D _rigidbody;
+
+    private Animator _animator; 
+
     private SpriteRenderer _renderer;
     GameManager _gameManager;
     public AudioClip hurtSound;
@@ -30,6 +33,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator> ();
         _renderer = GetComponent<SpriteRenderer>();
         _gameManager = GameObject.FindObjectOfType<GameManager>();
         _audioSource = GetComponent<AudioSource>();
@@ -75,6 +79,13 @@ public class Player : MonoBehaviour
     {
         float xSpeed = Input.GetAxis("Horizontal") * speed;
         _rigidbody.velocity = new Vector2(xSpeed,_rigidbody.velocity.y);
+
+        float xScale = transform.localScale.x;
+        if ((xSpeed < 0 && xScale > 0) || (xSpeed > 0 && xScale < 1))
+        {
+            transform.localScale *= new Vector2(-1,1);
+        }
+        _animator.SetFloat("Speed", Mathf.Abs(xSpeed));
     }
 
     void Update()
