23d58dd3bdb09ab45655d9869377a126d1be5294
Sabrina Li <swl416@nyu.edu>
2023-04-05 18:08:01 -0400

    created polar bear prefab + updated teleport audio in all game managers + updated lvl 4

--- Platformer/Assets/Code/IdleEnemy.cs
+++ Platformer/Assets/Code/IdleEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlackPenguin : MonoBehaviour
+public class IdleEnemy : MonoBehaviour
 {
     GameManager _gameManager;
     
